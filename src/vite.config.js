caa370417cf23871b518b0919f88839d1deb78b1
d.shkinin <amiddio.dev@gmail.com>
2024-08-02 18:00:47 +0400

    Changed vite config

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        strictPort: true,
+        port: 5173,
+        host: '0.0.0.0',
+        origin: 'http://crocuscms.local:5173',
+        hmr: {
+            host: 'crocuscms.local',
+        },
+    },
     plugins: [
         laravel({
             input: [
