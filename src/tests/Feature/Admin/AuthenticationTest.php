023a6dbc68d78dbb3d7ebe2e5672976b24c9d2b6
d.shkinin <amiddio.dev@gmail.com>
2024-08-13 17:24:23 +0400

    Added admin login/logout tests

---
+++ src/tests/Feature/Admin/AuthenticationTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Models\Admin\Admin;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+    private static string $adminPrefix = '';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        self::$adminPrefix = config('custom.admin_panel_prefix');
+    }
+
+    public function testLoginScreenCanBeRendered(): void
+    {
+        $response = $this->get(self::$adminPrefix . '/login');
+
+        $response->assertStatus(200);
+    }
+
+    public function testAdminCanAuthenticateUsingLoginScreen(): void
+    {
+        $admin = Admin::factory()->create();
+
+        $response = $this->post(self::$adminPrefix . '/login', [
+            'login' => $admin->login,
+            'password' => 'Qwerty#2024$',
+            'remember' => 1,
+        ]);
+
+        $this->assertAuthenticated(guard: 'admin');
+        $response->assertRedirect(route('admin.dashboard'));
+    }
+
+    public function testAdminCanNotAuthenticateWithInvalidPassword(): void
+    {
+        $admin = Admin::factory()->create();
+
+        $response = $this->post(self::$adminPrefix . '/login', [
+            'login' => $admin->login,
+            'password' => 'wrong-password',
+            'remember' => 1,
+        ]);
+
+        $this->assertGuest(guard: 'admin');
+    }
+
+    public function testAdminCanLogout(): void
+    {
+        $admin = Admin::factory()->create();
+
+        $response = $this->actingAs($admin)->post(self::$adminPrefix . '/logout');
+
+        $this->assertGuest(guard: 'admin');
+        $response->assertRedirect(route('admin.login'));
+    }
+
+}
